22aa93fd4c1d30b8ae7a4161b37505c1d5c68f6e
Cameron Martin <cameronmartin123@gmail.com>
2025-05-08 00:11:24 +0100

    Allocate page tables for kernel

--- crates/camos_uefi/src/main.rs
+++ crates/camos_uefi/src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::{fmt::Write, panic::PanicInfo};
+use core::{fmt::Write, panic::PanicInfo, ptr};
 
 use elf::{ElfBytes, ParseError, abi::PT_LOAD, endian::AnyEndian};
 use thiserror::Error;
@@ -11,17 +11,49 @@ use uefi::{
     prelude::entry,
     system,
 };
+use x86_64::{
+    structures::paging::{
+        mapper::PageTableFrameMapping, FrameAllocator, MappedPageTable, Mapper, Page, PageTable, PageTableFlags, PhysFrame, Size4KiB
+    }, PhysAddr, VirtAddr
+};
 
 static KERNEL: &[u8] = include_bytes!(env!("KERNEL_PATH"));
 
 #[derive(Error, Debug)]
 enum LoaderError {
     #[error("cannot parse elf: {0}")]
-    ElfParse(#[from] ParseError),
+    ElfParse(ParseError),
     #[error("the kernel elf was invalid")]
     InvalidKernelElf,
     #[error("invalid memory map")]
     InvalidMemoryMap,
+    #[error("allocation failed")]
+    FailedAllocation,
+}
+
+impl From<ParseError> for LoaderError {
+    fn from(value: ParseError) -> Self {
+        Self::ElfParse(value)
+    }
+}
+
+struct UefiFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for UefiFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        let frame_addr =
+            boot::allocate_pages(AllocateType::AnyPages, MemoryType::LOADER_DATA, 1).ok()?;
+
+        PhysFrame::from_start_address(PhysAddr::new(frame_addr.addr().get() as u64)).ok()
+    }
+}
+
+struct IdentityFrameMapping;
+
+unsafe impl PageTableFrameMapping for IdentityFrameMapping {
+    fn frame_to_pointer(&self, frame: PhysFrame) -> *mut PageTable {
+        VirtAddr::new(frame.start_address().as_u64()).as_mut_ptr()
+    }
 }
 
 #[entry]
@@ -30,25 +62,32 @@ fn efi_main() -> uefi::Status {
         return uefi::Status::WARN_WRITE_FAILURE;
     };
 
-    // TODO: Allocate the OS page tables before entering boot services!
-    boot::allocate_pages(AllocateType::AnyPages, MemoryType::LOADER_DATA, count);
-
     let memory_map = unsafe { boot::exit_boot_services(None) };
 
-    load_kernel(memory_map);
+    map_kernel();
+
+    // let entry_addr = elf_file.ehdr.e_entry;
+
+    // Jump to entry_addr
 
     uefi::Status::SUCCESS
 }
 
-/// Create a virtual memory mapping for the kernel executable then jumps to it
-fn load_kernel(memory_map: MemoryMapOwned) -> Result<(), LoaderError> {
-    // This is where the OS page tables will go
-    let first_physical_memory_section = memory_map
-        .entries()
-        .filter(|descriptor| descriptor.ty == MemoryType::CONVENTIONAL)
-        .next()
-        .ok_or(LoaderError::InvalidMemoryMap)?;
+/// Create a virtual memory mapping for the kernel executable
+fn map_kernel() -> Result<(), LoaderError> {
+    let mut frame_allocator = UefiFrameAllocator;
 
+    let level_4_table = IdentityFrameMapping.frame_to_pointer(
+        frame_allocator
+            .allocate_frame()
+            .ok_or(LoaderError::InvalidMemoryMap)?,
+    );
+    unsafe { ptr::write(level_4_table, PageTable::new()) };
+    let level_4_table = &mut unsafe { *level_4_table };
+
+    let mut mapper = unsafe { MappedPageTable::new(level_4_table, IdentityFrameMapping) };
+
+    let phys_kernel_start = PhysAddr::new(KERNEL.as_ptr() as u64);
     let elf_file = ElfBytes::<AnyEndian>::minimal_parse(KERNEL)?;
     let segments = elf_file.segments().ok_or(LoaderError::InvalidKernelElf)?;
 
@@ -57,18 +96,27 @@ fn load_kernel(memory_map: MemoryMapOwned) -> Result<(), LoaderError> {
         .filter(|segment| segment.p_type == PT_LOAD);
 
     for segment in load_segments {
-        let offset = segment.p_offset as usize;
-        let len = segment.p_filesz as usize;
-        let segment_bytes = &KERNEL[offset..offset + len];
+        let phys_start = phys_kernel_start + segment.p_offset;
+        let len = segment.p_filesz;
 
-        let virt_start = segment.p_vaddr;
+        let virt_start = VirtAddr::new(segment.p_vaddr);
         let virt_end = virt_start + segment.p_memsz;
 
-        let permissions = permissions_from_flags(segment.flags());
+        // let permissions = permissions_from_flags(segment.p_flags);
 
-        for frame in frame(phys_start)..=frame(phys_end - 1) {
-            // TODO: create page table mapping for frame with permissions
-            // at corresponding virtual address
+        for frame in PhysFrame::<Size4KiB>::range_inclusive(
+            PhysFrame::from_start_address(phys_start).map_err(|_| LoaderError::InvalidKernelElf)?,
+            PhysFrame::containing_address(phys_start + len - 1),
+        ) {
+            unsafe {
+                mapper.map_to(
+                    Page::from_start_address(virt_start)
+                        .map_err(|_| LoaderError::InvalidKernelElf)?,
+                    frame,
+                    PageTableFlags::PRESENT | PageTableFlags::GLOBAL,
+                    &mut frame_allocator,
+                )
+            };
         }
 
         // if virt_end > phys_end {
@@ -78,9 +126,7 @@ fn load_kernel(memory_map: MemoryMapOwned) -> Result<(), LoaderError> {
         // }
     }
 
-    let entry_addr = elf_file.ehdr.e_entry;
-
-    // Jump to entry_addr
+    Ok(())
 }
 
 #[panic_handler]
